148727125e2f8f93e55675df5f723855b9f0c2d6
Michael Sultzaberger <msultzaberger@unicon.net>
2020-10-14 06:57:06 -0700

    Updated the runtime folder to add the sample scripts and updated the README

---
+++ runtime/run.bat
@@ -0,0 +1,1 @@
+java -jar .\banner-connector-1.0.jar --spring.config.location=file:///C:/data/input/application.properties
---
+++ runtime/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar .\banner-connector-1.0.jar --spring.config.location=file:///data/input/application.properties
